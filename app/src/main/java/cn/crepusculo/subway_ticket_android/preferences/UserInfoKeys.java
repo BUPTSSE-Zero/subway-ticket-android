44006621fc78bd101dba459c52464a363e93e067
Graham Tang <airfree1452@gmail.com>
2016-07-05 14:37:53 +0800

    And settings fab

--- app/src/main/java/cn/crepusculo/subway_ticket_android/preferences/UserInfoKeys.java
+++ app/src/main/java/cn/crepusculo/subway_ticket_android/preferences/UserInfoKeys.java
@@ -12,13 +12,8 @@ public abstract class UserInfoKeys {
     // Register Info
     public static final String PHONE = "phone";
     public static final String PASSWORD = "password";
-    public static final String GENDER = "gender";
-    public static final String NICKNAME = "nickname";
-    public static final String SCHOOL = "school";
 
     // Personal Info
-    public static final String IS_SINGLE = "is_single";
-    public static final String LOVER = "lover";
     public static final String IS_FIRST_LOGIN = "is_first_login";
 
     private UserInfoKeys() {
