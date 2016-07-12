d9234f2e3ffe1b14e94e2aaa92423da4667c7a2f
Graham Tang <airfree1452@gmail.com>
2016-07-13 02:13:46 +0800

    Create select search class

---
+++ app/src/main/java/cn/crepusculo/subway_ticket_android/ui/activity/SelectActivity.java
@@ -0,0 +1,14 @@
+package cn.crepusculo.subway_ticket_android.ui.activity;
+
+import cn.crepusculo.subway_ticket_android.R;
+
+public class SelectActivity extends BaseActivity {
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.activity_select;
+    }
+
+    @Override
+    protected void initView() {
+    }
+}
