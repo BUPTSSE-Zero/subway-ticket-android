7cbe22655f1563fbf57945abe79086c3287554c9
Graham Tang <airfree1452@gmail.com>
2016-07-29 17:34:04 +0800

    Replace edit text open resource package

--- app/src/main/java/cn/crepusculo/subway_ticket_android/ui/activity/login/LoginActivity.java
+++ app/src/main/java/cn/crepusculo/subway_ticket_android/ui/activity/login/LoginActivity.java
@@ -1,9 +1,17 @@
 package cn.crepusculo.subway_ticket_android.ui.activity.login;
 
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
 import cn.crepusculo.subway_ticket_android.R;
 import cn.crepusculo.subway_ticket_android.ui.activity.BaseActivity;
 
 public class LoginActivity extends BaseActivity {
+
+    private LinearLayout card;
+    private EditText editTextUserName;
+    private EditText editTextPassword;
+
     @Override
     protected int getLayoutResource() {
         return R.layout.activity_login;
@@ -11,6 +19,14 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     protected void initView() {
+        initCard();
+    }
+
+    private void initCard() {
+        card = (LinearLayout) findViewById(R.id.card);
+        editTextUserName = (EditText) findViewById(R.id.edit_text_id);
+        editTextPassword = (EditText) findViewById(R.id.edit_text_pwd);
 
     }
+
 }
