f1363897e25e1fdb033f0eb492ebe2f7e628693e
Graham Tang <airfree1452@gmail.com>
2016-08-08 04:21:06 +0800

    Finish station display activity

--- app/src/main/java/cn/crepusculo/subway_ticket_android/ui/activity/StationDisplayActivity.java
+++ app/src/main/java/cn/crepusculo/subway_ticket_android/ui/activity/StationDisplayActivity.java
@@ -1,26 +1,50 @@
 package cn.crepusculo.subway_ticket_android.ui.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.dpizarro.uipicker.library.picker.PickerUI;
 import com.dpizarro.uipicker.library.picker.PickerUISettings;
+import com.google.gson.Gson;
+import com.subwayticket.database.model.SubwayLine;
+import com.subwayticket.database.model.SubwayStation;
+import com.subwayticket.model.result.SubwayLineListResult;
+import com.subwayticket.model.result.SubwayStationListResult;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import cn.crepusculo.subway_ticket_android.R;
+import cn.crepusculo.subway_ticket_android.content.Station;
+import cn.crepusculo.subway_ticket_android.ui.adapter.SearchHistoryAdapter;
+import cn.crepusculo.subway_ticket_android.ui.adapter.StationDisplayAdapter;
+import cn.crepusculo.subway_ticket_android.utils.GsonUtils;
+import cn.crepusculo.subway_ticket_android.utils.NetworkUtils;
 
 public class StationDisplayActivity extends BaseActivity {
     private RecyclerView recyclerView;
     private TextView progressTextView;
     private PickerUI pickerUI;
     private FloatingActionButton fab;
+
+    private int SEARCH_STATUS;
     private int id;
     private int shownFlag = 0;
+
+    private List<String> linesName;
+    private List<SubwayLine> lines;
+    private ArrayList<SubwayStation> stations;
 
     @Override
     protected int getLayoutResource() {
@@ -30,8 +54,10 @@ public class StationDisplayActivity extends BaseActivity {
     @Override
     protected void initView() {
         bindViews();
+        initLines();
         initPickUI();
         initFAB();
+        initRecycleView();
     }
 
     private void bindViews() {
@@ -39,6 +65,39 @@ public class StationDisplayActivity extends BaseActivity {
         progressTextView = (TextView) findViewById(R.id.progress_text_view);
         pickerUI = (PickerUI) findViewById(R.id.picker_ui_view);
         fab = (FloatingActionButton) findViewById(R.id.fab);
+    }
+
+    private void initLines() {
+        linesName = new ArrayList<>();
+        lines = new ArrayList<>();
+        NetworkUtils.subwayGetLineListByCity(
+                getString(R.string.BEIJING_ID),
+                new Response.Listener<SubwayLineListResult>() {
+                    @Override
+                    public void onResponse(SubwayLineListResult response) {
+                        lines = response.getSubwayLineList();
+                        for (SubwayLine line : lines
+                                ) {
+                            linesName.add(line.getSubwayLineName());
+                            pickerUI.setItems(StationDisplayActivity.this, linesName);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        /**
+                         * 获取失败
+                         */
+                        try {
+                            GsonUtils.Response r = GsonUtils.resolveErrorResponse(error);
+                            progressTextView.setText(r.result_description);
+                        } catch (NullPointerException e) {
+                            Snackbar.make(findViewById(R.id.view), "网络访问超时", Snackbar.LENGTH_LONG).show();
+                            progressTextView.setText("网络访问超时");
+                        }
+                    }
+                });
     }
 
     private void initPickUI() {
@@ -65,7 +124,7 @@ public class StationDisplayActivity extends BaseActivity {
                         shownFlag -= 1;
                         Log.e("flag", shownFlag + "");
                         if (shownFlag <= 1) {
-                            Toast.makeText(StationDisplayActivity.this, mValueResult + " " + mPosition, Toast.LENGTH_SHORT).show();
+                            shownFlag = 0;
                             id = mPosition;
                             pickerUI.slide();
                             fab.setVisibility(View.VISIBLE);
@@ -73,6 +132,9 @@ public class StationDisplayActivity extends BaseActivity {
                                     StationDisplayActivity.this, R.anim.fade_in_center
                                     )
                             );
+                            if (!lines.isEmpty()) {
+                                reloadRecycleView();
+                            }
                         }
                     }
                 }, 1500);
@@ -97,5 +159,86 @@ public class StationDisplayActivity extends BaseActivity {
                 }
             }
         });
+    }
+
+    private void initRecycleView() {
+        stations = new ArrayList<>();
+
+        if (!lines.isEmpty()) {
+            reloadRecycleView();
+        }
+
+        if (!stations.isEmpty()) {
+            RecyclerView.LayoutManager layoutManager;
+            layoutManager = new LinearLayoutManager(this);
+
+            recyclerView.setLayoutManager(layoutManager);
+            recyclerView.setHasFixedSize(true);
+            StationDisplayAdapter adapter = new StationDisplayAdapter(
+                    StationDisplayActivity.this,
+                    stations,
+                    new StationDisplayAdapter.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(int position, SubwayStation data, int mode) {
+                            switch (mode) {
+                                case SearchHistoryAdapter.STATUS_COME:
+                                    setSearchResult(Station.SubwayStationAdapter(data));
+                                    break;
+                                case SearchHistoryAdapter.STATUS_GO:
+                                    setSearchResult(Station.SubwayStationAdapter(data));
+                                    break;
+
+                            }
+                            finish();
+                        }
+                    });
+            recyclerView.setAdapter(adapter);
+        }
+    }
+
+
+    private void reloadRecycleView() {
+
+        NetworkUtils.subwayGetStationByLine(
+                "" + lines.get(id).getSubwayLineId(),
+                new Response.Listener<SubwayStationListResult>() {
+                    @Override
+                    public void onResponse(SubwayStationListResult response) {
+                        stations = new ArrayList<>(response.getSubwayStationList());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        try {
+                            GsonUtils.Response r = GsonUtils.resolveErrorResponse(error);
+                            progressTextView.setText(r.result_description);
+                        } catch (NullPointerException e) {
+                            progressTextView.setText("网络访问超时");
+                        }
+                    }
+                }
+        );
+    }
+
+    private void setSearchResult(Station s) {
+        Gson gson = new Gson();
+        Bundle result = new Bundle();
+        result.putString(
+                SEARCH_STATUS == SearchActivity.EDIT_TEXT_REQUEST_CODE_START ?
+                        SearchActivity.KEY_START : SearchActivity.KEY_END,
+                gson.toJson(s));
+        putIntent(result);
+    }
+
+    private void putIntent(Bundle b) {
+        Intent intent = new Intent(StationDisplayActivity.this, MainActivity.class);
+        intent.putExtras(b);
+        if (SEARCH_STATUS == SearchActivity.EDIT_TEXT_REQUEST_CODE_START)
+            setResult(SearchActivity.EDIT_TEXT_REQUEST_CODE_START, intent);
+        else if (SEARCH_STATUS == SearchActivity.EDIT_TEXT_REQUEST_CODE_END)
+            setResult(SearchActivity.EDIT_TEXT_REQUEST_CODE_END, intent);
+        else
+            setResult(SearchActivity.EDIT_TEXT_REQUEST_CODE_BOTH, intent);
     }
 }
---
+++ app/src/main/java/cn/crepusculo/subway_ticket_android/ui/adapter/StationDisplayAdapter.java
@@ -0,0 +1,83 @@
+package cn.crepusculo.subway_ticket_android.ui.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.subwayticket.database.model.SubwayStation;
+
+import java.util.ArrayList;
+
+import cn.crepusculo.subway_ticket_android.R;
+
+public class StationDisplayAdapter extends RecyclerView.Adapter<StationDisplayAdapter.ViewHolder> {
+    public static final int STATUS_COME = 1;
+    public static final int STATUS_GO = 2;
+
+    private final LayoutInflater mInflater;
+    public Context context;
+    public ArrayList<SubwayStation> stationArrayList;
+    private OnItemClickListener listener;
+
+    public StationDisplayAdapter(Context context, ArrayList<SubwayStation> stationArrayList, OnItemClickListener listener) {
+        super();
+        mInflater = LayoutInflater.from(context);
+        this.context = context;
+        this.listener = listener;
+        this.stationArrayList = stationArrayList;
+    }
+
+    @Override
+    public int getItemCount() {
+        return stationArrayList.size();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        final View itemView = mInflater.inflate(R.layout.item_row_history_station, parent, false);
+        Log.e("onCreateViewHolder", "inflate item_row_history_station");
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        final SubwayStation station = (SubwayStation) stationArrayList.get(position);
+        holder.txtName.setText(station.getSubwayStationName());
+        holder.goToThere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onItemClick(position, stationArrayList.get(position), STATUS_GO);
+            }
+        });
+        holder.comeFromThere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onItemClick(position, stationArrayList.get(position), STATUS_COME);
+            }
+        });
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(int position, SubwayStation data, int mode);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView txtName;
+        private Button goToThere;
+        private Button comeFromThere;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            txtName = (TextView) itemView.findViewById(R.id.txtName);
+            goToThere = (Button) itemView.findViewById(R.id.goToThere);
+            comeFromThere = (Button) itemView.findViewById(R.id.comeFromThere);
+        }
+    }
+
+}
