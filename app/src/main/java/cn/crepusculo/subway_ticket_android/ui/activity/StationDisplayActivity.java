4eff12fb433ea189be5671e37abc351e03a8284c
Graham Tang <airfree1452@gmail.com>
2016-08-07 15:27:37 +0800

    Build station display activity

---
+++ app/src/main/java/cn/crepusculo/subway_ticket_android/ui/activity/StationDisplayActivity.java
@@ -0,0 +1,16 @@
+package cn.crepusculo.subway_ticket_android.ui.activity;
+
+import cn.crepusculo.subway_ticket_android.R;
+
+public class StationDisplayActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.activity_station_display;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
