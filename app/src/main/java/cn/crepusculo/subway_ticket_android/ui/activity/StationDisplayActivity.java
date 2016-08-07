409a5985760b337b82a7c20ba277aec79912dcfe
Graham Tang <airfree1452@gmail.com>
2016-08-08 01:39:39 +0800

    Create Station display activity

--- app/src/main/java/cn/crepusculo/subway_ticket_android/ui/activity/StationDisplayActivity.java
+++ app/src/main/java/cn/crepusculo/subway_ticket_android/ui/activity/StationDisplayActivity.java
@@ -1,8 +1,17 @@
 package cn.crepusculo.subway_ticket_android.ui.activity;
 
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+import com.dpizarro.uipicker.library.picker.PickerUI;
+import com.dpizarro.uipicker.library.picker.PickerUISettings;
+
 import cn.crepusculo.subway_ticket_android.R;
 
 public class StationDisplayActivity extends BaseActivity {
+    private RecyclerView recyclerView;
+    private TextView progressTextView;
+    private PickerUI pickerUI;
 
     @Override
     protected int getLayoutResource() {
@@ -11,6 +20,21 @@ public class StationDisplayActivity extends BaseActivity {
 
     @Override
     protected void initView() {
+        bindViews();
+        initPickUI();
 
+    }
+
+    private void bindViews() {
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        progressTextView = (TextView) findViewById(R.id.progress_text_view);
+        pickerUI = (PickerUI) findViewById(R.id.picker_ui_view);
+    }
+
+    private void initPickUI() {
+        PickerUISettings pickerUISettings = new PickerUISettings.Builder()
+                .build();
+        pickerUI.setSettings(pickerUISettings);
+        pickerUI.slide();
     }
 }
